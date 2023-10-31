95c1310396ab75f19a35cb4f1b84fae465214dc4
Joao Soares <jsoareez@gmail.com>
2023-10-31 18:21:44 -0300

    chore: fix tailwind classes, add clsx & tailwind merge, finish error validation

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -36,16 +36,16 @@ export function Login() {
         <Input
           type="email"
           placeholder="E-mail"
+          error={errors.email?.message}
           {...register('email')}
         />
-        {errors.email && <span>{errors.email.message}</span>}
 
         <Input
           type="password"
           placeholder="Senha"
+          error={errors.password?.message}
           {...register('password')}
         />
-        {errors.password && <span>{errors.password.message}</span>}
 
         <Button type="submit" className="mt-2">
           Entrar
