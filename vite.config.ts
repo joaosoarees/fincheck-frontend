8e677ac404bf67e3b39fcb061527aafffdb9ea41
Joao Soares <jsoareez@gmail.com>
2023-11-06 00:24:39 -0300

    chore: vite splitVendorChunk plugin to optimize build bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
   server: {
     open: true,
   },
