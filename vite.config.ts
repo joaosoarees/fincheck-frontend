b8c02ff683986fe334e84163ed21957a7840420c
Joao Soares <jsoareez@gmail.com>
2023-11-06 00:29:24 -0300

    chore: move vite packages to dev dependencies & comment eslint import tule

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
