013e16794502d76318098e8ba771d258aae7986e
Joao Soares <jsoareez@gmail.com>
2023-11-07 22:26:45 -0300

    feat: start NewAccountModal form

--- src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,4 +1,7 @@
+import { Input } from '@view/components/Input';
+import { InputCurrency } from '@view/components/InputCurrency';
 import { Modal } from '@view/components/Modal';
+import { Select } from '@view/components/Select';
 
 import { useNewAccountModalController } from './useNewAccountModalController';
 
@@ -12,7 +15,22 @@ export function NewAccountModal() {
       open={isNewAccountModalVisible}
       onClose={handleCloseNewAccountModal}
     >
-      NewAccountModal
+      <form>
+        <div>
+          <span className="text-gray-600 tracking-[-0.5px] text-xs">Saldo</span>
+
+          <div className="flex items-center gap-2">
+            <span className="text-gray-600 tracking-[-0.5px] text-lg">R$</span>
+            <InputCurrency />
+          </div>
+        </div>
+
+        <div className="mt-10 ">
+          <Input type="text" name="name" placeholder="Nome da conta" />
+
+          <Select />
+        </div>
+      </form>
     </Modal>
   );
 }
